0e3afb825cdf0ab6a2a3e8f96ec8985f78109973
slittleton <littletonltp@gmail.com>
2019-06-17 09:36:48 -0700

    added minor styling to landing

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,43 +1,50 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { setTrainersList } from '../actions';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { setTrainersList } from "../actions";
 
 class Landing extends Component {
   async componentDidMount() {
     // const trainersList = JSON.parse(localStorage.getItem("trainers"));
 
-    const trainersList = ['Picky', 'Ash', 'Misty']
+    const trainersList = ["Picky", "Ash", "Misty"];
 
-    if( trainersList ){
+    if (trainersList) {
       await this.props.setTrainersList(trainersList);
     }
-
   }
 
-  trainersList(trainersList){
+  loginTrainer(trainer) {}
 
-    if(trainersList){
-      return trainersList.map(trainer => {
+  trainersList(trainersList) {
+    if (trainersList) {
       return (
-      <div key={trainer}>
-        {trainer}
-      
-      </div>)
-    })
-      
-      
+        <div className="trainer-list-wrapper">
+          {trainersList.map((trainer, index) => {
+            return (
+              <div
+                key={index}
+                className="trainer-item"
+                onClick={() => this.loginTrainer(trainer)}
+              >
+                {index + 1} {trainer}
+              </div>
+            );
+          })}
+        </div>
+      );
     }
-
   }
-
 
   render() {
     return (
-      <div>
-        <div>Select A Trainer</div>
-        {this.trainersList(this.props.trainersList)}
+      <div className="landing ">
+        <div className="trainer-title">Select A Trainer</div>
+        <div className="trainer-list-box">{this.trainersList(this.props.trainersList)}</div>
+        <div className="centering-box">
+          <div className="trainer-google"> Login With Google </div>
+        </div>
       </div>
-    )
+    );
   }
 }
 const mapStateToProps = state => {
@@ -45,11 +52,13 @@ const mapStateToProps = state => {
   return {
     trainersList: state.trainerReducer.trainersList,
     currentTrainer: state.trainerReducer.currentTrainer
-  }
-  
-}
+  };
+};
 
-export default  connect(mapStateToProps, { setTrainersList })(Landing)
+export default connect(
+  mapStateToProps,
+  { setTrainersList }
+)(Landing);
 
 /*
 retreive saved trainers and show list 
